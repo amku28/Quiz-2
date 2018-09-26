e9788b86aec03e8cd761fe5e67cebca1f60521dc
Alexander Ming Ku <kuam@s.dcsdk12.org>
2018-09-26 09:48:15 -0600

    Add simulation of 1000 thrice turns

---
+++ Thrice1000.java
@@ -0,0 +1,63 @@
+import java.lang.Math;
+
+public class Thrice1000 {
+    public static void main(String[] args) {
+        double avg = 0;
+
+        for (int turn = 1; turn <= 1000; turn ++) {
+
+            double count = 1;
+            int dice1 = (int) (Math.random()*6 + 1);
+            int dice2 = (int) (Math.random()*6 + 1);
+            int dice3 = (int) (Math.random()*6 + 1);
+            int value = dice1 + dice2 + dice3;
+            double tally = value;
+
+            while (dice1 != dice2 && dice2 != dice3) {
+                count++;
+                dice1 = (int) (Math.random()*6 + 1); 
+                dice2 = (int) (Math.random()*6 + 1);
+                dice3 = (int) (Math.random()*6 + 1);
+                value = dice1 + dice2 + dice3;
+
+                if (count % 3 == 0) {
+                    tally = tally - value;
+                } else {
+                    tally = tally + value;
+                }
+            }
+
+            int tripletWorth = dice1;
+            for (count = 1; count <= tripletWorth; count++) {
+                dice1 = (int) (Math.random()*6 + 1); 
+                dice2 = (int) (Math.random()*6 + 1);
+                dice3 = (int) (Math.random()*6 + 1);
+                value = dice1 * dice2 * dice3;
+                tally = tally + value;
+            }
+
+            count = 0;
+            while ((dice1 != dice2 && dice2 != dice3)) {
+                dice1 = (int) (Math.random()*6 + 1); 
+                dice2 = (int) (Math.random()*6 + 1);
+                dice3 = (int) (Math.random()*6 + 1);
+
+                tally = tally + (Math.pow(3, count));
+                count++;
+            }
+
+            if (dice1 == tripletWorth) {
+                tally = tally * 3;
+            } else {
+                tally = dice1;
+            }
+
+            avg = avg + tally;
+            System.out.println("After turn " + turn + " your tally is " + tally);
+        }
+
+        avg = avg / 1000;
+        System.out.println("The average tally for 1000 turns is " + avg);
+
+    }
+}
